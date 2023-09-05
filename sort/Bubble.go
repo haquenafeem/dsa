2d7bce01d8b1d77693aee2d7c0fb1b9c266ef516
haquenafeem <haquenafeem@gmail.com>
2023-09-05 12:55:20 +0600

    updates

---
+++ sort/Bubble.go
@@ -0,0 +1,14 @@
+package sort
+
+func Bubble(arr []int) []int {
+	length := len(arr)
+	for i := 0; i < length-1; i++ {
+		for j := 0; j < length-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+
+	return arr
+}
